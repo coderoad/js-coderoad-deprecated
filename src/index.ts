8947d9fc1ae14518d636c580a502d8815fdf1bae
ShMcK <shawn.j.mckay@gmail.com>
2016-08-21 13:56:33 -0700

    fix file path

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const jsCodeRoad = ({dir, content}) => (`
 (function(){'use strict';
 require('babel-register')({plugins:[['transform-es2015-modules-commonjs',{loose:true,sourceRoot:'${dir}'}]]});
 ${require('process-console-log').logger}
-let _fileExists = require('node-file-exists');
-let _resolve = require('fs').resolve;
+let _fileExists = require('node-file-exists').default;
+let _resolve = require('path').resolve;
 global.exists = (p) => _fileExists(_resolve('${dir}',p));
 require = require('rewire-coderoad');
 
